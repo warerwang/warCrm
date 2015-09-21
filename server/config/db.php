26bc8e743b3f35105c1aa4d791635ef523069093
yadongw <warerwang@126.com>
2015-09-22 01:08:06 +0800

    修改数据库配置。

--- server/config/db.php
+++ server/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=warcrm',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'warcrm',
+    'password' => 'warcrm',
     'charset' => 'utf8',
 ];
